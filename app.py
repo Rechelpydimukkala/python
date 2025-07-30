810ce18e49bbed2e873c9c4bb8fce524ac492ef1
Rechelpydimukkala <143074892+Rechelpydimukkala@users.noreply.github.com>
2025-07-31 00:15:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,4 @@ def hello():
     return "Welcome to Jenkins Tutorials"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
