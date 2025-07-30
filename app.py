fa277b01cf1ed6f49e3065e4fdc163e41c7005c7
Rechelpydimukkala <143074892+Rechelpydimukkala@users.noreply.github.com>
2025-07-31 00:04:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import os
-print("DEBUG: os module loaded")
-
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -10,5 +8,6 @@ def home():
     return "<h1>Welcome to Jenkins Tutorials</h1>"
 
 if __name__ == '__main__':
+    print("DEBUG: os module is loaded")
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
