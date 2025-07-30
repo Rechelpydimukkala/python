d58b6c6979b3d7517fc5e345a753b51607feca41
Rechelpydimukkala <143074892+Rechelpydimukkala@users.noreply.github.com>
2025-07-31 00:06:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def home():
 
 if __name__ == '__main__':
     print("DEBUG: os module is loaded")
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
